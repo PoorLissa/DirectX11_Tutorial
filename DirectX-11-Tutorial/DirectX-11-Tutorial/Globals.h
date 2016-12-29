cc93801091543b9a56d3b9855f93fac24f6c410d
PoorLissa <poortwistme@gmail.com>
2016-12-29 23:29:00 +0300

    2d - new icon for plasma gun

--- DirectX-11-Tutorial/DirectX-11-Tutorial/Globals.h
+++ DirectX-11-Tutorial/DirectX-11-Tutorial/Globals.h
@@ -5,7 +5,7 @@
 // Let us put our global defines here, which is actually a poor choice since cnahging this global defines rebuilds all the project
 // ------------------------------------------------------------------------------------------------------------------------
 
-#define MONSTERS_QTY             3
+#define MONSTERS_QTY             3000
 
 #define fullScreen
 #undef  fullScreen
